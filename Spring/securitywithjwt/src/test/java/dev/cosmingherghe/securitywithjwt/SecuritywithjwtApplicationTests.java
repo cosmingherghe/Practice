d27eb38b285ca80a0dad809ffda408faed1bb6b3
cosmingherghe <cosminlinux@gmail.com>
2022-10-05 23:05:21 -0400

    This is not a commit

---
+++ Spring/securitywithjwt/src/test/java/dev/cosmingherghe/securitywithjwt/SecuritywithjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cosmingherghe.securitywithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritywithjwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
